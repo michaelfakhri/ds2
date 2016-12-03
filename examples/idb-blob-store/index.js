aec063b7b709d3e4ea01f9c83c43eddd6241781c
michaelfakhri <fakhrimichael@live.com>
2016-12-03 04:43:47 -0500

    INIT

--- examples/idb-blob-store/index.js
+++ examples/idb-blob-store/index.js
@@ -5,10 +5,10 @@ config.db.config.clear();
 config.exists("config",function(err,data){console.log(data)});
 stream.pull(
 	stream.once(JSON.stringify({id:"1",priv:"2",pub:"3"})),
-	config.write('config',function(err){if(err)console.log(err)}))
+	config.write('config',function(err){if(err)console.log(err)})
+)
 //stream.once(JSON.stringify({id:"1",priv:"2",pub:"3"}));
-setTimeout(function(){config.exists("config",function(err,data){
-	console.log(data)
+setTimeout(function(){config.exists("config",function(err,exists){
 	stream.pull(
 		config.read("config",function(data,err){console.log(data)}),
 		stream.pullDecode(),
