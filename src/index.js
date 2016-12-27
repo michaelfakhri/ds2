96314655a2ab4be7b17c9949546a51cd82ad18d4
Michael Fakhry <fakhrimichael@live.com>
2016-12-27 05:31:51 -0500

    removed uncessary tracked files and Promise -> deferred

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const Libp2PIpfsBrowser = require('libp2p-ipfs-browser')
 const MultihashingAsync = require('multihashing-async')
 const stream = require('pull-stream')
 const Logger = require('logplease')
+const deferred = require('deferred')
 Logger.setLogLevel(Logger.LogLevels.DEBUG) // change to ERROR
 
 const logger = Logger.create('UP2P', { color: Logger.Colors.Blue })
@@ -26,62 +27,44 @@ module.exports = class UniversalPeerToPeer {
       // aPeerId = undefined
     }
     let self = this
-    return new Promise((resolve, reject) => {
-      if (aPeerId) {
-        resolve(aPeerId)
-      } else {
-        self.db.configExists()
-        .then(function (exists) {
-          if (exists) {
-            self.db.getConfig()
-            .then((peerInfo) => {
-              PeerId.createFromJSON(peerInfo, (err, peerId) => {
-                if (err) return reject(err)
-                resolve(peerId)
-              })
-            })
-          } else {
-            PeerId.create((err, peerId) => {
-              if (err) return reject(err)
+    let def = deferred()
+
+    if (aPeerId) {
+      def.resolve(aPeerId)
+    } else {
+      self.db.configExists()
+      .then((exists) => {
+        if (exists) {
+          self.db.getConfig()
+            .then((peerIdJSON) => deferred.promisify(PeerId.createFromJSON)(peerIdJSON))
+            .then((peerId) => def.resolve(peerId))
+        } else {
+          deferred.promisify(PeerId.create)()
+            .then((peerId) => {
               self.db.storeConfig(peerId.toJSON())
-              .then(() => resolve(peerId))
+                .then(() => def.resolve(peerId))
             })
-          }
-        })
-      }
-    })
-    .then((peerId) => {
-      return new Promise((resolve, reject) => {
-        this.db.setupFileStorage(peerId.toB58String())
-        var peerInfo = new PeerInfo(peerId)
-        self.node = new Libp2PIpfsBrowser(peerInfo)
-        var ma = '/libp2p-webrtc-star/ip4/127.0.0.1/tcp/15555/ws/ipfs/' + peerId.toB58String()
-        peerInfo.multiaddr.add(ma)
-        logger.debug('YOU CAN REACH ME AT ID = ' + peerId.toB58String())
-        self.connectionHandler = new ConnectionHandler(self.node, self.db)
-        resolve(self)
+        }
       })
+    }
+    return def.promise.then((peerId) => {
+      this.db.setupFileStorage(peerId.toB58String())
+      var peerInfo = new PeerInfo(peerId)
+      self.node = new Libp2PIpfsBrowser(peerInfo)
+      var ma = '/libp2p-webrtc-star/ip4/127.0.0.1/tcp/15555/ws/ipfs/' + peerId.toB58String()
+      peerInfo.multiaddr.add(ma)
+      logger.debug('YOU CAN REACH ME AT ID = ' + peerId.toB58String())
+      self.connectionHandler = new ConnectionHandler(self.node, self.db)
+      return deferred(self)
     })
   }
 
   start () {
-    let self = this
-    return new Promise((resolve, reject) => {
-      self.node.start((err) => {
-        if (err) return reject(err)
-        resolve(self)
-      })
-    })
+    return deferred.promisify(this.node.start.bind(this.node))()
   }
 
   stop () {
-    let self = this
-    return new Promise((resolve, reject) => {
-      self.node.stop((err) => {
-        if (err) return reject(err)
-        resolve(self)
-      })
-    })
+    return deferred.promisify(this.node.stop.bind(this.node))()
   }
 
   connect (aUserHashStr) {
@@ -92,22 +75,20 @@ module.exports = class UniversalPeerToPeer {
   disconnect (aUserHashStr) {
     return this.connectionHandler.disconnect(aUserHashStr)
   }
-
   publish (aData, aMetadata) {
     var self = this
-    return new Promise((resolve, reject) => {
-      MultihashingAsync(Buffer(aData), 'sha2-256', (err, mh) => {
-        if (err) reject(err)
+    return deferred.promisify(MultihashingAsync)(Buffer(aData), 'sha2-256')
+      .then((mh) => {
         var hash = MultihashingAsync.multihash.toB58String(mh)
+        var def = deferred()
         stream(
           stream.once(aData),
           self.db.getFileWriter(hash, function () {
-            if (err) return reject(err)
-            resolve(hash)
+            def.resolve(hash)
           })
         )
+        return def.promise
       })
-    })
   }
 
   view (aDataHashStr) {
--- unrelatedBadExamples/README.md
+++
@@ -1,1 +0,0 @@
-<h1 align="center">IN PROGRESS</h1>
